659b909777c2dc3cf8f8da7df8ae82763ecd1788
SnowpawStudios <sayajjin@hotmail.com>
2022-09-06 17:30:40 +1000

    Client-side basic auth complete

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +12,16 @@ export class AppComponent implements OnInit {
   title = 'The Social app';
   users: any; 
   
-  constructor(private http:HttpClient){}
+  constructor(private accountService: AccountService){}
 
   ngOnInit(): void {
-    this.getUsers();
+    
+    this.setCurrentUser();
   }
-
-  getUsers(){
-    this.http.get('https://localhost:7126/api/users').subscribe(response =>
-    {
-      this.users = response;
-      console.log(response);
-    });
+  setCurrentUser()
+  {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
-
-  
+   
 }
